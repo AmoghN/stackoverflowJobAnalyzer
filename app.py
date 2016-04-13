43e78d37963ddcedf3fcf1f93d84ff12d2a3c021
Amogh Nayak <amoghnk03@gmail.com>
2016-04-12 22:07:45 -0400

    Heroku Fix
    Removing debug from flask

--- app.py
+++ app.py
@@ -39,5 +39,5 @@ def drawGraph():
             ,searchTerm = searchTerm
             ,success = success)
 
-if __name__ == "__main__":
-    app.run(debug=True)
+# if __name__ == "__main__":
+#     app.run(debug=True)
