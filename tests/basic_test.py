e8f1ec6c6a67a10e0aaa74f487e1fdaa2080b542
Amogh Nayak <amogh03@gmail.com>
2018-12-29 21:36:00 -0500

    update circle config file
    added python query test

--- tests/basic_test.py
+++ tests/basic_test.py
@@ -4,3 +4,8 @@ def test_home_page():
     r = requests.get('http://localhost:80')
 
     assert(r.status_code == 200)
+
+def test_query_python():
+    r = requests.get('http://localhost:80/search?q=python')
+
+    assert(r.status_code == 200)
